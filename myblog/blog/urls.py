0ae066522e2e6d9680db0db01f1d52f1e193aca8
parkhan <voftt1@gmail.com>
2022-07-05 01:21:31 +0900

    initial

--- myblog/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'blog'
-urlpatterns = [
-    path('', views.blog_index, name='index'),
-    path('detail/<int:id>/', views.blog_detail, name='detail'),
-    path('<category>/', views.blog_category, name='category'),
-]
--- myblog/project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'project'
-urlpatterns = [
-    path('', views.project_index, name='index'),
-    path('detail/<int:id>', views.project_detail, name='detail'),
-]
