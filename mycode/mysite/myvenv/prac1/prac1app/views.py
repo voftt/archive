02cf34372ce370fc60dddfc863330e3f869c3192
parkhan <voftt1@gmail.com>
2022-07-05 01:23:13 +0900

    remove

--- mycode/mysite/myvenv/prac1/prac1app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return render(request, 'index.html')
-
-def count(request):
-    full_text = request.GET["fulltext"]
-    return render(request,'count.html',{'text':full_text, 'total':len(full_text)})
